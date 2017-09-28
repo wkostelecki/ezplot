e42b5671b6c1ae02bf3d23902568663d0f3210b4
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2017-09-28 08:03:34 +0100

    extended agg_data

--- R/agg_data.R
+++ R/agg_data.R
@@ -16,14 +16,23 @@ agg_data = function(data,
                     group_by = not_numeric(x),
                     agg_fun = function(x) sum(x, na.rm = TRUE)){
 
-  x = bind_cols(lapply(nameifnot(cols),
+  COLS = unpack_cols(cols)
+
+  x = bind_cols(lapply(nameifnot(unique(unname(c(COLS[[2]],
+                                                 unlist(COLS[[3]]))))),
                        function(x) eval(parse(text = x),
                                         envir = data)))
 
-  x %>%
+  x = x %>%
     group_by(!!!syms(group_by)) %>%
     summarize_all(agg_fun) %>%
-    ungroup
+    as.data.frame
+
+  m = match(names(x), COLS[[1]])
+
+  x = x[, !is.na(m), drop = FALSE]
+  names(x) = names(COLS[[1]])[m[!is.na(m)]]
+  x
 
 }
 
