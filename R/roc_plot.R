65b6d1960b51605db562c885c9af193ddd320f74
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-01-14 09:00:33 +0000

    updates to roc_plot

--- R/roc_plot.R
+++ R/roc_plot.R
@@ -5,7 +5,7 @@
 #' roc_plot(actual, fitted)
 #'
 #' @export
-roc_plot = function(actual, fitted){
+roc_plot = function(actual, fitted, size = 20){
 
   pred = ROCR::prediction(fitted, actual)
   perf = ROCR::performance(pred, "tpr", "fpr")
@@ -14,11 +14,15 @@ roc_plot = function(actual, fitted){
   g = ggplot(data.frame(x = perf@x.values[[1]],
                         y = perf@y.values[[1]])) +
     geom_line(aes(x, y)) +
+    geom_line(data = data.frame(x = c(0, 1), y = c(0, 1)),
+              aes(x, y),
+              linetype = 2) +
     coord_equal() +
-    theme_minimal() +
+    theme_minimal(size) +
     xlab('False Positive Rate') +
     ylab('True Positive Rate') +
-    ggtitle(paste0("AUC = ", signif(auc, 3)))
+    ggtitle(paste0("AUC = ", signif(auc, 3))) +
+    theme(plot.title = element_text(hjust = 0.5))
 
   g
 
