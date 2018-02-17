b3f96171651d2eb169e5e1abc3e3f5447a6cbceb
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-02-17 15:01:02 +0000

    added documentation added option for ascending/descending ordering

--- R/reorder_levels.R
+++ R/reorder_levels.R
@@ -1,18 +1,33 @@
 
-
-
-
+#' Order levels of factor columns using fct_reorder
+#'
+#' @param data A data.frame.
+#' @param cols Names of columns to reorder.
+#' @param y Numeric column for order priority.
+#' @param .desc Default is TRUE for all columns in \code{cols}
+#'
+#' @return
+#'
+#' @examples
+#' reorder_levels(mtcars, "cyl", "1") %>% str
+#' reorder_levels(mtcars, "cyl", "1", FALSE) %>% str
+#' reorder_levels(mtcars, "cyl", "mpg") %>% str
 reorder_levels = function(data,
                           cols = c("group", "facet_x", "facet_y"),
-                          y = "y"){
+                          y = "y",
+                          .desc = rep(TRUE, length(cols))){
 
   reorder = forcats::fct_reorder
-  for (this_col in cols){
+
+  for (i in seq_along(cols)){
+    this_col = cols[i]
     if (!is.null(data[[this_col]])){
-      data[[this_col]] = reorder(data[[this_col]],
-                                 data[[y]],
-                                 function(x) sum(x, na.rm = TRUE),
-                                 .desc = TRUE)
+      data[[this_col]] = reorder(
+        factor(data[[this_col]]),
+        if (y == "1") rep(1, nrow(data)) else data[[y]],
+        function(x) sum(x, na.rm = TRUE),
+        .desc = .desc[i]
+      )
     }
   }
 
