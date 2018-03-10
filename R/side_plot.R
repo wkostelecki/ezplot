8c31b635d9eb4bb6e77707fab365654aeddbadc6
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-03-10 05:23:25 +0000

    updates for side_plot to show multiple metrics

--- R/side_plot.R
+++ R/side_plot.R
@@ -9,30 +9,68 @@
 #'
 #' @export
 #' @examples
-#' side_plot(fruit, "Product", "Units")
-#' side_plot(fruit, "Store", "Units")
+#' side_plot(mtcars, "gear", "1")
+#' side_plot(mtcars, "cyl", "hp <= 120")
+#' side_plot(mtcars, "cyl", c(count = "ifelse(cyl == 4, 1, -1)", "hp <= 120"))
 side_plot = function(data,
                      x,
                      y,
                      ylabels = ez_labels,
                      size = 20,
-                     palette = ez_col){
-
-
+                     palette = ez_col,
+                     signif = 3){
+  # browser()
+  y = nameifnot(y)
+  y_names = names(y)
   cols = c(x = unname(x),
-           y = unname(y))
+           setNames(y, paste0("y", seq_along(y))))
 
   gdata = agg_data(data,
-                   cols)
+                   cols,
+                   group_by = cols["x"])
 
-  ggplot(gdata) +
+  gdata[["x"]] = factor(gdata[["x"]])
+
+  gdata = gdata %>%
+    mutate(x = fct_reorder(x, y1, function(x) sum(x, na.rm = TRUE)))
+
+  gdata = gdata %>%
+    tidyr::gather(facet_x, y, -x) %>%
+    mutate(facet_x = y_names[as.numeric(forcats::fct_inorder(facet_x))])
+
+  gdata = gdata %>%
+    group_by(facet_x) %>%
+    mutate(label_offset = 0.02 * diff(range(c(y, 0), na.rm = TRUE)) * ifelse(y >= 0, 1, -1)) %>%
+    ungroup
+
+  expand = c(
+    if (min(gdata[["y"]], na.rm = TRUE) < 0) 0.2 else 0,
+    0,
+    if (max(gdata[["y"]], na.rm = TRUE) > 0) 0.2 else 0,
+    0
+  )
+
+  g = ggplot(gdata) +
     geom_col(aes(x, y),
              fill = palette(1)) +
-    scale_y_continuous(labels = ez_labels,
-                       expand = c(0, 0, 0.3, 0)) +
-    coord_flip() +
+    geom_text(aes(x, y + label_offset,
+                  label = ez_labels(y, signif = signif),
+                  hjust = ifelse(y >= 0, 0, 1)),
+              vjust = 0.5,
+              size = size / 3.5,
+              colour = "grey30") +
+    geom_text(aes(x, 1.2 * y, label = "")) +
+    scale_y_continuous(labels = ez_labels) +
+    coord_flip(expand = FALSE) +
     theme_ez(size) +
     xlab(NULL) +
-    ylab(NULL)
+    ylab(NULL) +
+    theme(panel.grid.major.y = element_blank(),
+          panel.grid.major.x = element_line(colour = "grey85",
+                                            size = if (size > 16) 0.8 else 0.2),
+          axis.line.y = element_line(color = "grey85",
+                                     size = if (size > 16) 0.8 else 0.2),
+          strip.placement = "outside")
+  quick_facet(g, strip.position = "bottom", scales = "free_x")
 
 }
