c9eb583eb208fabd7a5ed61b84c23ed4cd376fb2
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-05-15 11:41:28 +0100

    updates to ensure get_incr always always uses seconds for POSIXct dates

--- R/get_incr.R
+++ R/get_incr.R
@@ -1,13 +1,18 @@
 
-
-
-
+#' get_incr
+#' @description returns the minimun increment between sorted unique values of a
+#'   vector
+#' @param x A numeric or date vector
 get_incr = function(x){
 
-  y = as.numeric(unique(diff(sort(unique(x))))[1])
-
-  if (lubridate::is.POSIXct(x)){
-    y = y * 86400
+  if (lubridate:::is.POSIXct(x)) {
+    y = sort(unique(x))
+    y = as.numeric(min(difftime(y,
+                                dplyr::lag(y),
+                                units = "seconds"),
+                       na.rm = TRUE))
+  } else {
+    y = as.numeric(min(diff(sort(unique(x)))))
   }
 
   y
