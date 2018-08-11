0022719a8da3046aa8818d481ca91c88c9f714e3
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-08-11 18:45:56 +0100

    fixed

--- data-raw/README-images.R
+++ data-raw/README-images.R
@@ -1,7 +1,7 @@
 ez_png(line_plot(df, "year", "value", "fct"),
-       height = 200,
-       width = 300,
-       resx = 2,
+       height = 150,
+       width = 250,
+       resx = 1.5,
        # check = FALSE,
        "man/figures/README-line_plot.png",
        dir.create = TRUE)
