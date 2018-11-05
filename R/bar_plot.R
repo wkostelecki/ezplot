080f738b3dd4bb6a317116936d1c387f9c2de663
Wojtek Kostelecki <wkostelecki@gmail.com>
2018-11-05 17:49:23 +0000

    argument renaming and label position option

--- R/bar_plot.R
+++ R/bar_plot.R
@@ -1,7 +1,7 @@
 #' bar_plot
 #' @inheritParams area_plot
 #' @param width Width of bar.
-#' @param y_rescale Rescaling factor for y-axis limits
+#' @param rescale_y Rescaling factor for y-axis limits
 #' @param label_cutoff Cutoff size (proportion of limit range) for excluding labels
 #'
 #' @return A ggplot object.
@@ -10,13 +10,12 @@
 #' @examples
 #' df = ez_data()
 #' bar_plot(df, "year", "units")
+#' bar_plot(df, "year", c("Share of Units" = "units"), "fct", position = "fill")
 #' bar_plot(df, "year", "units", "fct", use_theme = ggplot2::theme_bw)
-#' bar_plot(df, "year", "units", "fct", reorder = NULL)
-#' bar_plot(df, "year", "units", "fct", reorder = NULL, position = "fill")
-#' bar_plot(df, "year", c(Units = "units"))
+#' bar_plot(df, "year", "units", "fct", reorder = NULL, label_pos = "both")
 #' bar_plot(df, "year", c(Units = "units"), "fct", "char")
-#' bar_plot(df, "year", "units", "fct", "char", "num", position = "fill")
-#' bar_plot(df, "year", "ifelse(fct == 'X', units, -units)", "fct")
+#' bar_plot(df, "year", "units", "fct", "char", "num")
+#' bar_plot(df, "year", "ifelse(fct == 'X', units, -units)", "fct", label_pos = "both")
 bar_plot = function(data,
                     x,
                     y,
@@ -32,11 +31,14 @@ bar_plot = function(data,
                     } else {
                       ez_labels
                     },
-                    y_rescale = 1,
+                    label_pos = c("auto", "inside", "top", "both"),
+                    rescale_y = 1.1,
                     label_cutoff = 0.12,
                     use_theme = theme_ez,
                     position = "stack",
                     facet_scales = "fixed") {
+
+  label_pos = match.arg(label_pos)
 
   y = nameifnot(y)
 
@@ -60,6 +62,16 @@ bar_plot = function(data,
     gdata[["group"]] = forcats::fct_rev(gdata[["group"]])
   }
 
+  if (label_pos == "auto") {
+    if (position == "fill") {
+      label_pos = "inside"
+    } else if (!exists("group", gdata) || length(unique(gdata[["group"]])) == 1) {
+      label_pos = "top"
+    } else {
+      label_pos = "inside"
+    }
+  }
+
   if (position == "fill") {
     gdata = gdata %>%
       group_by(!!!syms(setdiff(group_vars, "group"))) %>%
@@ -78,7 +90,7 @@ bar_plot = function(data,
     group_by(!!!syms(c(setdiff(group_vars, "group"), "sign"))) %>%
     mutate(y_height = sum(y)) %>%
     group_by(!!!syms(setdiff(group_vars, c("group", "x", cutoff_groups)))) %>%
-    mutate(y_range = diff(range(y_height, 0)) * (1 + (1 - y_rescale) * n_distinct(sign))) %>%
+    mutate(y_range = diff(range(y_height, 0)) * (1 + (1 - rescale_y) * n_distinct(sign))) %>%
     ungroup
 
   gdata = gdata  %>%
@@ -92,6 +104,8 @@ bar_plot = function(data,
 
   g = ggplot(gdata)
 
+
+
   if ("group" %in% names(gdata)){
 
     fill_pal = rev(palette(length(unique(gdata[["group"]]))))
@@ -102,31 +116,58 @@ bar_plot = function(data,
       scale_fill_manual(NULL,
                         values = fill_pal,
                         labels = function(x) paste0(x, "   "),
-                        breaks = rev) +
-      geom_text(aes(x, ylabel_pos,
-                    label = ylabel_text,
-                    colour = group),
-                size = size / 4) +
-      scale_colour_manual(NULL,
-                          values = text_contrast(fill_pal),
-                          guide = "none")
+                        breaks = rev)
 
   } else {
     fill_pal = palette(1)
     g = g +
       geom_col(aes(x, y),
                fill = fill_pal,
-               width = width) +
-      geom_text(aes(x, ylabel_pos,
-                    label = ylabel_text),
-                colour = text_contrast(fill_pal),
-                size = size / 4)
+               width = width)
+  }
+
+  if (label_pos %in% c("inside", "both")) {
+    if ("group" %in% names(gdata)){
+      g = g +
+        geom_text(aes(x, ylabel_pos,
+                      label = ylabel_text,
+                      colour = group),
+                  size = size / 4) +
+        scale_colour_manual(NULL,
+                            values = text_contrast(fill_pal),
+                            guide = "none")
+    } else {
+      g = g +
+        geom_text(aes(x, ylabel_pos,
+                      label = ylabel_text),
+                  colour = text_contrast(fill_pal),
+                  size = size / 4)
+    }
+  }
+
+  if (label_pos %in% c("top", "both")) {
+    top_labels = gdata %>%
+      group_by_(.dots = intersect(names(gdata),
+                                  c("x", "facet_x", "facet_y"))) %>%
+      summarize(top_y = sum(y[y > 0], na.rm = TRUE),
+                y = sum(y, na.rm = TRUE)) %>%
+      ungroup %>%
+      mutate(top_ylabel_text = labels_y(signif(y, 3)))
+
+    g = g +
+      geom_text(data = top_labels,
+                aes(x, top_y,
+                    label = top_ylabel_text),
+                size = size / 4,
+                vjust = -0.5)
   }
 
   g = quick_facet(g, scales = facet_scales)
 
-  expand = c(0.1 * any(gdata[["y"]] < 0), 0,
-             0.1 * any(gdata[["y"]] >= 0), 0)
+  expand = c((rescale_y - 1) * any(gdata[["y"]] < 0) * (position == "stack"),
+             0,
+             (rescale_y - 1) * any(gdata[["y"]] >= 0) * (position == "stack"),
+             0)
 
   g +
     xlab(names(x)) +
