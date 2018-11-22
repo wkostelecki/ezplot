dc593841e47aabde8de221c36965c85d93fbb834
Wojtek Kostelecki <wkostelecki@gmail.com>
2018-11-22 00:39:26 +0000

    update roc_plot for group/facet_x/facet_y options

--- R/roc_plot.R
+++ R/roc_plot.R
@@ -6,22 +6,72 @@
 #' @examples
 #' library(ggplot2)
 #' n = 10000
-#' actual = sample(c(FALSE, TRUE), n, replace = TRUE)
-#' roc_plot(actual, actual)
-#' roc_plot(actual, runif(n))
-#' fitted = runif(n) ^ ifelse(actual == 1, 0.5, 2)
-#' ggplot(data.frame(actual, fitted)) +
+#' df = data.frame(actual = sample(c(FALSE, TRUE), n, replace = TRUE),
+#'                 runif = runif(n))
+#' df[["fitted"]] = runif(n) ^ ifelse(df[["actual"]] == 1, 0.5, 2)
+#'
+#' ggplot(df) +
 #'   geom_density(aes(fitted, fill = actual), alpha = 0.5)
-#' roc_plot(actual, fitted)
-roc_plot = function(actual, fitted, size = 12){
+#'
+#' roc_plot(df, "actual", "actual")
+#' roc_plot(df, "actual", "fitted")
+#' roc_plot(df, "actual", "runif")
+#'
+#' roc_plot(df, "actual", "fitted", "sample(c(1, 2), n(), TRUE)")
+#'
+#' roc_plot(df, "actual", "fitted",
+#'          "sample(c(1, 2), n(), TRUE)",
+#'          "sample(c(1, 2), n(), TRUE)")
+#'
+#' roc_plot(df, "actual", "fitted",
+#'          "sample(c(1, 2), n(), TRUE)",
+#'          "sample(c(1, 2), n(), TRUE)",
+#'          "sample(c(1, 2), n(), TRUE)")
+#'
+roc_plot = function(data, actual, fitted,
+                    group = NULL,
+                    facet_x = NULL,
+                    facet_y = NULL,
+                    size = 12){
 
-  pred = ROCR::prediction(as.numeric(fitted), actual)
-  perf = ROCR::performance(pred, "tpr", "fpr")
-  auc = methods::slot(ROCR::performance(pred, "auc"), "y.values")[[1]]
+  cols = c(actual = unname(actual),
+           fitted = unname(fitted),
+           group = unname(group),
+           facet_x = unname(facet_x),
+           facet_y = unname(facet_y))
 
-  g = ggplot(data.frame(x = perf@x.values[[1]],
-                        y = perf@y.values[[1]])) +
-    geom_line(aes(x, y)) +
+  data = data %>%
+    ungroup %>%
+    transmute_(.dots = cols)
+
+  total = data %>%
+    tibble::as.tibble() %>%
+    summarize(roc = list(roc(actual, fitted)))
+
+  gdata = data %>%
+    group_by_(.dots = intersect(names(cols),
+                                c("group", "facet_x", "facet_y"))) %>%
+    summarize(roc = list(roc(actual, fitted))) %>%
+    ungroup %>%
+    tidyr::unnest(roc)
+
+  g = ggplot(gdata)
+
+  if (exists("group", gdata)) {
+    g = g +
+      geom_line(aes(x = false_positive,
+                    y = true_positive,
+                    colour = factor(group))) +
+      scale_colour_manual(NULL, values = ez_col(n_distinct(gdata[["group"]])))
+  } else {
+    g = g +
+      geom_line(aes(x = false_positive,
+                    y = true_positive))
+  }
+
+  g = quick_facet(g)
+
+  g = g +
     geom_line(data = data.frame(x = c(0, 1), y = c(0, 1)),
               aes(x, y),
               linetype = 2) +
@@ -29,11 +79,47 @@ roc_plot = function(actual, fitted, size = 12){
     theme_minimal(size) +
     xlab('False Positive Rate') +
     ylab('True Positive Rate') +
-    ggtitle(paste0("AUC = ", signif(auc, 3))) +
+    scale_y_continuous(labels = ez_labels) +
+    scale_x_continuous(labels = ez_labels) +
+    ggtitle(paste0("AUC = ", signif(total[["roc"]][[1]][["auc"]][1], 3))) +
     theme(plot.title = element_text(hjust = 0.5))
 
   g
 
 }
 
+
+#' @examples
+#' roc(sample(c(T, F), 1, replace = TRUE), runif(1))
+#' roc(sample(c(T, F), 3, replace = TRUE), runif(3))
+roc = function(actual, fitted) {
+
+  ind = !is.na(actual) & !is.na(fitted)
+  actual = actual[ind]
+  fitted = fitted[ind]
+
+  count = sum(actual == actual[1])
+  if (sum(ind) == 0 | count == 0 | count == length(actual)) {
+    return(
+      data.frame(
+        true_positive = NA,
+        false_positive = NA,
+        auc = NA
+      )
+    )
+  }
+
+  pred = ROCR::prediction(as.numeric(fitted), actual)
+  perf = ROCR::performance(pred, "tpr", "fpr")
+  auc = methods::slot(ROCR::performance(pred, "auc"), "y.values")[[1]]
+
+  x = perf@x.values[[1]]
+  y = perf@y.values[[1]]
+
+
+  data.frame(false_positive = x,
+             true_positive = y,
+             auc = auc)
+
+}
 
