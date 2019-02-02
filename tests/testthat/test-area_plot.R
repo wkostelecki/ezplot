7c1d80c5643e325c372d367fd001710b717a6583
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-02-02 20:49:19 +0000

    added tests

---
+++ tests/testthat/test-area_plot.R
@@ -0,0 +1,9 @@
+context("area_plot")
+
+test_that("area_plot works", {
+  g = area_plot(mtcars, "cyl", "1")
+
+  expect_equal(g[["data"]]$x, c(4, 6, 8))
+  expect_equal(g[["data"]]$y, c(11, 7, 14))
+
+})
