e846a4b7c5d8227793e42fb15d48d986d0596616
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-04-14 20:11:32 +0100

    fixed test

--- tests/testthat/test_agg_data2.R
+++ tests/testthat/test_agg_data2.R
@@ -2,15 +2,20 @@
 
 context("agg_data2")
 
-test_that("", {
+test_that("agg_data works", {
 
-  agg_data(mtcars,
-           c(x = "factor(cyl)", y = "hp", z = "factor(am)"),
-           group_by = c(x = "factor(cyl)", z = "factor(am)"))
+  df = agg_data(mtcars,
+                c(x = "factor(cyl)", y = "hp", z = "factor(am)"),
+                group_by = c(x = "factor(cyl)", z = "factor(am)"))
 
-  agg_data(mtcars,
-           c(x = "factor(cyl)", y = "hp", z = "factor(am)"),
-           group_by = c(z = "factor(am)", x = "factor(cyl)"))
+  df_expected = mtcars %>%
+    dplyr::group_by(x = factor(cyl), z = factor(am)) %>%
+    dplyr::summarize(y = sum(hp, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(x, y, z) %>%
+    as.data.frame()
+
+  expect_equal(df, df_expected)
 
 })
 
