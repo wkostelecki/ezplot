0566df95a19db55a42de48feaaa9ee079e4b6a8b
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-05-01 21:26:09 +0100

    updates to agg_data function

--- R/agg_data.R
+++ R/agg_data.R
@@ -1,10 +1,10 @@
 #' Aggregates data
-#'
 #' @param data A data.frame.
 #' @param cols Named character vector of column names.
-#' @param group_by Vector of grouping column names.
+#' @param group_by Vector of grouping columns.
 #' @param agg_fun Function to use for aggregating.
-#' @param group_by2 Vector of grouping column names to use for delayed (post aggregation) calculation.
+#' @param group_by2 Vector of grouping column names to use for delayed (post
+#'   aggregation) calculation.
 #' @param env Environment for extra variables.
 #' @return An aggregated data.frame.
 #' @importFrom rlang syms
@@ -17,6 +17,7 @@
 #' agg_data(df, "value", "fct")
 #' agg_data(df, "value", c("fct", "year"))
 #' agg_data(df, c(x = "year", y = "value"), c(x = "year"))
+#' agg_data2(mtcars, c(x = "cyl", y = "1", group = "cyl"), c(x = "cyl", group = "cyl"))
 agg_data = function(data,
                     cols = names(data),
                     group_by = NULL,
@@ -29,7 +30,6 @@ agg_data = function(data,
   first_expr = nameifnot(unique(unname(c(COLS[["direct"]],
                                          group_by,
                                          unlist(COLS[["indirect_vars"]])))))
-
   x = transmute(data,
                 !!!lapply(first_expr,
                           function(x) rlang::parse_quo(x, env = env)))
@@ -43,20 +43,14 @@ agg_data = function(data,
 
   group_by = nameifnot(group_by)
 
-  if (length(group_by) == 0) {
-    group_by = c(".group." = "1")
-  } else {
-    group_by = stats::setNames(paste0("`", group_by, "`"),
-                               names(group_by))
+  COLS[["groups"]] = group_by
+
+  if (length(group_by) > 0) {
+    names(group_by) = paste0(".agg_group.", names(group_by), ".agg_group.")
   }
 
-  names(group_by) = paste0(".agg_group.", names(group_by), ".agg_group.")
-
   x = x %>%
-    mutate_(.dots = group_by[!(names(group_by) %in% names(x))])
-
-  x = x %>%
-    group_by(!!!syms(names(group_by))) %>%
+    group_by(!!!syms(group_by[!(names(group_by) %in% names(x))])) %>%
     summarize_if(function(x) is.numeric(x) | is.logical(x), agg_fun) %>%
     as.data.frame(check.names = FALSE)
 
@@ -70,21 +64,19 @@ agg_data = function(data,
 
     x = x %>%
       group_by(!!!syms(group_by2)) %>%
-      mutate_(.dots = COLS[["indirect_expr"]]) %>%
+      mutate(!!!lapply(COLS[["indirect_expr"]],
+                       function(x) rlang::parse_quo(x, env = env))) %>%
       as.data.frame
 
   }
 
   group_by_ = gsub("`", "", group_by)
   names(group_by_) = gsub("\\.agg_group\\.", "", names(group_by_))
-
   take = COLS[["cols"]]
-  ind = which(match(take, group_by_) == match(names(take), names(group_by_)))
-
-  pos = which(take[ind] %in% group_by_ & names(take)[ind] %in% names(group_by_))
-
-  take[ind] = names(group_by)[pos]
-
+  ind = names(take) %in% names(COLS[["groups"]])
+  if (any(ind)) {
+    take[ind] = paste0(".agg_group.", names(take[ind]), ".agg_group.")
+  }
   m = match(take, names(x))
 
   x = x[, m, drop = FALSE]
