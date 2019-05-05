6ae7b2c6a1b91cf6e67c1a86918e70568d4c3d30
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-05-05 01:28:53 +0100

    more tests

--- tests/testthat/test-ez_png.R
+++ tests/testthat/test-ez_png.R
@@ -2,17 +2,21 @@ context("ez_png")
 
 test_that("ez_png works", {
   ez_png(ggplot2::ggplot(),
-         "test.png")
+         "test.png",
+         check = FALSE)
   expect_true(file.exists("test.png"))
   unlink("test.png")
 
   expect_error(ez_png(ggplot2::ggplot(),
-                      "temp/test.png"))
+                      "temp/test.png",
+                      check = FALSE))
 
   ez_png(ggplot2::ggplot(),
          "temp/test.png",
-         dir.create = TRUE)
+         dir.create = TRUE,
+         check = FALSE)
   expect_true(file.exists("temp/test.png"))
   unlink("temp/test.png")
+  unlink("temp")
 
 })
