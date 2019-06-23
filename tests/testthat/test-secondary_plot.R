f5a8ed93dfe3ef08c248a99edeb6764a2f1d8ad1
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-06-23 20:28:15 +0100

    - updated secondary_plot test - small fixes

--- tests/testthat/test-secondary_plot.R
+++ tests/testthat/test-secondary_plot.R
@@ -3,4 +3,8 @@ context("secondary_plot")
 test_that("base function works", {
   g = secondary_plot(mtcars, "cyl", "mpg", "hp")
   expect_equal(dim(g$data), c(3, 3))
+
+  g = secondary_plot(mtcars, "as.character(cyl)", "mpg", "hp")
+  expect_equal(dim(g$data), c(3, 4))
+
 })
