0ddaab024df2f7ddc2e06c0d440ee036c8f2434c
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-06-25 20:59:14 +0100

    update for new warnings?

--- tests/testthat/test-ez_png.R
+++ tests/testthat/test-ez_png.R
@@ -7,9 +7,11 @@ test_that("ez_png works", {
   expect_true(file.exists("test.png"))
   unlink("test.png")
 
-  expect_error(ez_png(ggplot2::ggplot(),
+  suppressWarnings(
+    expect_error(ez_png(ggplot2::ggplot(),
                       "temp/test.png",
                       check = FALSE))
+  )
 
   ez_png(ggplot2::ggplot(),
          "temp/test.png",
