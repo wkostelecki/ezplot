9286b713a2ab3b72f5e19c483868884bb75899e1
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-07-02 23:25:43 +0100

    added calendar plot

---
+++ R/calendar_plot.R
@@ -0,0 +1,35 @@
+#' calendar_plot
+#' @inheritParams area_plot
+#' @param x date column
+#' @param ... additional arguments for tile_plot
+#' @export
+#' @examples
+#' library(tsibbledata)
+#' calendar_plot(vic_elec, "Time", "Demand", zlim = c(NA, NA))
+calendar_plot = function(data, x, y, ...) {
+  wd = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+
+
+  cols = c(x = paste0("lubridate::floor_date(", unname(x), ", 'day')"),
+           y = unname(y))
+
+  gdata = agg_data(
+    data, cols, cols["x"]
+  )
+
+  gdata = gdata %>%
+    mutate(wday = factor(wd[lubridate::wday(x)], wd),
+           week = lubridate::epiweek(x),
+           year = lubridate::year(x),
+           month = factor(month.name[lubridate::month(x)], month.name),
+           week = ifelse(month == "December" & week == 1, 53, week)) %>%
+    group_by(year, month) %>%
+    mutate(month_week = week - min(week) + 1) %>%
+    ungroup
+
+  tile_plot(gdata, "wday", "month_week", "y", "year", "month", reorder = NULL, ...) +
+    scale_y_reverse(labels = function(x) rep("", length(x)))
+
+
+
+}
