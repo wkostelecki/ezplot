61f2d47c52df11eca4c7c6d1281b315ed29fe0c0
Wojtek Kostelecki <wkostelecki@gmail.com>
2019-08-12 12:44:17 +0100

    added unit tests

---
+++ tests/testthat/test-variable_plot.R
@@ -0,0 +1,25 @@
+
+context("variable_plot")
+
+
+test_that("variable_plot line geom works", {
+
+  g = variable_plot(tsibbledata::pelt, "Year", c("Lynx", "Hare"))
+
+  expect_equal(2 * nrow(tsibbledata::pelt), nrow(g[["data"]]))
+
+  expect_error(variable_plot(tsibbledata::ansett, "Week", "Passengers", facet_x = "Class", yoy = TRUE), NA)
+
+})
+
+
+test_that("variable_plot bar geom works", {
+
+  expect_error(variable_plot(tsibbledata::hh_budget, "Year",
+                             c("Debt\n(% of disposable income)" = "Debt",
+                               "Expenditure\nGrowth (%)" = "Expenditure",
+                               "Unemployment (%)" = "Unemployment"),
+                             facet_x = "Country", geom = "bar"),
+               NA)
+
+})
