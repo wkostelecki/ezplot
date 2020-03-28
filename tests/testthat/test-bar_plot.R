4025e790d47d6bfb3a8d3f547a7bd273f65f2a87
Wojtek Kostelecki <wkostelecki@gmail.com>
2020-03-28 12:15:39 +0000

    test for coord_flip option in bar_plot

--- tests/testthat/test-bar_plot.R
+++ tests/testthat/test-bar_plot.R
@@ -16,4 +16,13 @@ test_that("bar_plot works", {
   g = bar_plot(mtcars, "cyl", "1", position = "fill")
   expect_equal(g$data$y, c(1, 1, 1))
 
+  g = bar_plot(mtcars, "factor(cyl)", "1", coord_flip = TRUE)
+  expect_equal(g$data$x, factor(c(4, 6, 8), c(8, 6, 4)))
+  expect_equal(g$data$y, c(11, 7, 14))
+
+  g = bar_plot(mtcars, "cyl", "1", coord_flip = TRUE)
+  expect_equal(g$data$x, c(4, 6, 8))
+  expect_equal(g$data$y, c(11, 7, 14))
+
+
 })
