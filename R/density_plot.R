b76d8e900837bc5886c0b578be3cf7479999af82
Wojtek Kostelecki <wkostelecki@gmail.com>
2020-05-07 22:44:35 +0100

    cleaned up and documented density_plot

--- R/density_plot.R
+++ R/density_plot.R
@@ -1,18 +1,19 @@
-
-
 #' density_plot
 #' @description creates a densityi plot
+#' @inheritParams area_plot
+#' @param adjust multiplicate bandwidth adjustment
+#' @param alpha alpha
 #' @export
 #' @examples
 #' density_plot(mtcars, "wt", "cyl")
 #' density_plot(subset(tsibbledata::olympic_running, Length == "100m" & Year >= 1980),
-#'              "Time", "Year - Year %% 10", "Sex", facet_scales = "free", ncol = 1, adjust = 2)
+#'              "Time", "Year - Year %% 10", "Sex", facet_scales = "free", facet_ncol = 1, adjust = 2)
 density_plot = function(data, x, group = NULL, facet_x = NULL,
                         facet_y = NULL,
                         adjust = 1,
                         alpha = 0.5,
                         facet_scales = "fixed",
-                        ncol = NULL,
+                        facet_ncol = NULL,
                         env = parent.frame()) {
 
   cols = c(x = unname(x),
@@ -39,7 +40,7 @@ density_plot = function(data, x, group = NULL, facet_x = NULL,
 
   }
 
-  quick_facet(g, scales = facet_scales, ncol = ncol) +
+  quick_facet(g, scales = facet_scales, ncol = facet_ncol) +
     theme_ez() +
     scale_y_continuous(labels = ez_labels, expand = c(0, 0)) +
     xlab(names(nameifnot(x)))
