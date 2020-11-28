f1454facbe8c0dbe69b8dbc90c3927becac91f30
Wojtek Kostelecki <wkostelecki@gmail.com>
2020-11-28 16:56:45 +0000

    added quantile option to perf_df()

--- R/performance.R
+++ R/performance.R
@@ -74,11 +74,42 @@ pred = function(fitted, actual) {
 #' @export
 #' @examples
 #' perf_df(mtcars$mpg, mtcars$am)
+#' perf_df(mtcars$mpg, mtcars$am, quantiles = 4)
+#' perf_df(mtcars$mpg, mtcars$am, quantiles = 10)
 #' perf_df(mtcars$wt, mtcars$am==0)
-perf_df = function(fitted, actual) {
+perf_df = function(fitted, actual, quantiles = NULL) {
+  if (!is.null(quantiles)) {
+
+
+
+    quantiles = min(quantiles, length(fitted))
+
+    bins = pmax(ceiling(seq(0, quantiles, length.out = length(fitted))), 1)
+
+    r = rank(fitted, ties.method = "first")
+    # o = order(fitted, decreasing = TRUE)
+
+    # q = quantile(r, probs = seq(0, 1, length.out = quantiles + 1))
+
+    x = data.frame(r,
+                   # o,
+                   fitted,
+                   # c = cut(r, q, include.lowest = T),
+                   cc = bins[r]) %>%
+      # dplyr::group_by(c) %>%
+      # dplyr::mutate(qfitted = min(r)) %>%
+      dplyr::group_by(c) %>%
+      dplyr::mutate(qfitted = min(r))
+
+    fitted = x$qfitted
+    ocutoffs = x %>%
+      dplyr::group_by(qfitted) %>%
+      dplyr::summarize(cutoff = min(fitted),
+                       .groups = "drop")
+  }
   pred = pred(fitted, actual)
-  # performance(pred, )
-  df = data.frame(cutoffs = pred@cutoffs[[1]],
+
+  df = data.frame(cutoff = pred@cutoffs[[1]],
                   fp = pred@fp[[1]],
                   tp = pred@tp[[1]],
                   tn = pred@tn[[1]],
@@ -86,14 +117,25 @@ perf_df = function(fitted, actual) {
                   pp = pred@n.pos.pred[[1]],
                   np = pred@n.neg.pred[[1]])
 
-  for (metric in c("rpp", "acc", "fpr", "tpr", "fnr", "tnr", "prec", "lift", "auc", "aucpr")) {
+  extra_metrics = c("rpp", "acc", "fpr", "tpr", "fnr", "tnr", "prec", "lift", "auc", "aucpr")
+  if (!is.null(quantiles)) extra_metrics = setdiff(extra_metrics, c("auc", "aucpr"))
+
+  for (metric in extra_metrics) {
 
     df[[metric]] = ROCR::performance(pred, metric)@y.values[[1]]
 
   }
-  df %>%
-    dplyr::mutate(ks = abs(fpr - tpr)) %>%
-    dplyr::arrange(desc(cutoffs))
+  df = df %>%
+    dplyr::mutate(ks = abs(fpr - tpr),
+                  f1 = 2 * prec * tpr / (prec + tpr))
+  if (!is.null(quantiles)) {
+    df = df %>%
+      utils::tail(quantiles) %>%
+      dplyr::mutate(cutoff = rev(ocutoffs$cutoff)) %>%
+      cbind(quantile = seq_len(quantiles), .) %>%
+      identity()
+  }
+  df
 }
 
 globalVariables(c("tpr", "fpr", "cutoffs"))
